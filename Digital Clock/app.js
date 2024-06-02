119dbc816232af7d73cb7ad38418198e798e6cee
Srushti-1515 <srushtichaudhari28gmail.com>
2024-06-02 19:17:04 +0530

    Initaial changes

---
+++ Digital Clock/app.js
@@ -0,0 +1,25 @@
+window.onload=() => {
+
+function digitalClock(){
+const date =new Date();
+
+let hours =date.getHours();
+let minutes =date.getMinutes();
+let seconds= date.getSeconds();
+
+hours= hours <10 ? "0" + hours:hours;
+minutes= minutes <10 ? "0" + minutes:minutes;
+seconds=seconds <10 ? "0" + seconds:seconds;
+
+
+document.querySelector('#clock-hours').innerText = hours;
+document.querySelector('#clock-min').innerText = minutes;
+document.querySelector('#clock-sec').innerText = seconds;
+
+
+setTimeout(digitalClock, 1000);
+}
+
+digitalClock();
+
+}
